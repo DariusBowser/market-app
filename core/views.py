47d125bc474d6405827014a0bbbfcbcc57de6913
Darius Bowser <134982506+DariusBowser@users.noreply.github.com>
2023-12-23 19:07:45 -0500

    sign up form

--- core/views.py
+++ core/views.py
@@ -2,17 +2,27 @@ from django.shortcuts import render
 
 from item.models import Item, Category
 
+from .forms import SignupForm
+
 
 def index(request):
-
     items = Item.objects.filter(is_sold=False)[0:6]
     categories = Category.objects.all()
 
-    return render(request, "core/index.html",
-{
-    'categories': categories,
-    'items': items,
-})
+    return render(
+        request,
+        "core/index.html",
+        {
+            "categories": categories,
+            "items": items,
+        },
+    )
+
 
 def contact(request):
     return render(request, "core/contact.html")
+
+
+def signup(request):
+    form = SignupForm()
+    return render(request, "core/signup.html", {"form": form})
